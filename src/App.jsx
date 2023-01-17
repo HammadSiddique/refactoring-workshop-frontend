cb017130c3cecf1f66ceb18f75fb85ab35325cdd
HammadSiddique <hammad.siddique01@gmail.com>
2023-01-17 23:40:58 +0500

    Update import statements after reafactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react"
 import { useSelector } from "react-redux"
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
-import { Footer } from "./components/footer/Footer"
-import { Header } from "./components/header/Header"
-import { Home } from "./components/Home"
-import { Account } from "./components/Account"
-import { Login } from "./components/Login"
-import { Register } from "./components/Register"
+import { Footer } from "./reusable_components/footer/Footer"
+import { Header } from "./reusable_components/header/Header"
+import { Home } from "./pages/home/Home"
+import { Account } from "./pages/account/Account"
+import { Login } from "./pages/register/login/Login"
+import { Register } from "./pages/register/signup/Register"
 
 const App = () => {
   const authenticated = useSelector((state) => state.auth.authenticated)
